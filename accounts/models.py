5d35513266d007637cac2b6db7b8fa591cc8bd9a
이재희/UCube가입서비스개발팀 <87461594+Hui-Story@users.noreply.github.com>
2026-07-19 17:05:40 +0900

    feat: Member email 필드에 unique 제약 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class Member(AbstractUser):
-    pass
+    email = models.EmailField("이메일", unique=True, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        # 빈 문자열을 NULL로 정규화해 email 미입력 회원 간 unique 충돌을 방지한다.
+        self.email = self.email or None
+        super().save(*args, **kwargs)
